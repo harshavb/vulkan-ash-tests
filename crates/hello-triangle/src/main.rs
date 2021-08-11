d4b5d5bb5c42d7aec90aa2c550404ebceecba955
harshavb <48032800+harshavb@users.noreply.github.com>
2021-08-11 16:53:22 -0700

    Unwrap TriangleApplication instead of matching
    Unwrap has pretty much the exact same functionality

--- crates/hello-triangle/src/main.rs
+++ crates/hello-triangle/src/main.rs
@@ -3,9 +3,6 @@ mod hello_triangle;
 use hello_triangle::TriangleApplication;
 
 fn main() {
-    let (app, io) = match TriangleApplication::new() {
-        Ok((app, io)) => (app, io),
-        Err(error) => panic!("{}", error),
-    };
+    let (app, io) = TriangleApplication::new().unwrap();
     hello_triangle::run(app, io);
 }
