b93a4c6aa8a944eab631ab567523111cf76b2568
harshavb <48032800+harshavb@users.noreply.github.com>
2021-08-10 18:56:22 -0700

    Get a window with a graceful cleanup working

--- crates/hello-triangle/src/main.rs
+++ crates/hello-triangle/src/main.rs
@@ -3,10 +3,7 @@ mod hello_triangle;
 use hello_triangle::TriangleApplication;
 
 fn main() {
-    match TriangleApplication::run() {
-        Ok(string) => println!("{}", string),
-        Err(error) => println!("{}", error),
-    }
-
-    println!("test");
+    let (app, io) = TriangleApplication::new().unwrap();
+    
+    hello_triangle::run(app, io);
 }
